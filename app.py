e1f42e04ca2ac173a9db012aeeac23f61565f23d
Orb-H <orb.cov@gmail.com>
2023-11-09 03:53:47 +0000

    Default Flask project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/robots.txt')
+def robot():
+  return '''User-Agent: *
+Disallow: /
+'''
+
+if __name__=="__main__":
+  app.run(host="0.0.0.0", debug=True)
