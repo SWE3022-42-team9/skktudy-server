0f46bdd6a898191c8521c4c4caa7dfde82cc1334
Orb-H <orb.cov@gmail.com>
2023-11-13 20:22:18 +0900

    Change tab size to 4

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/robots.txt')
 def robot():
-  return '''User-Agent: *
+    return '''User-Agent: *
 Disallow: /
 '''
 
 if __name__=="__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
