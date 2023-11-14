2ab995df53333bb4a6933e7a50c357b69124c06b
Orb-H <orb.cov@gmail.com>
2023-11-14 19:36:29 +0900

    Add board_list related db functions

--- util/db.py
+++ util/db.py
@@ -4,6 +4,8 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from models import *
 
+from typing import List
+
 engine = create_engine('') # TODO: Add database URL
 Session = sessionmaker(bind=engine)
 
@@ -30,3 +32,25 @@ def _execute_sql(sql: str) -> dict | SQLAlchemyError:
 # --------------------------------------------------
 
 # etc.
+
+# offset과 limit으로 지정된 범위의 Board의 목록을 반환합니다
+def get_board_list(offset: int, limit: int) -> List[Board] | SQLAlchemyError:
+    try:
+        session = Session()
+        res = session.query(Board) \
+                .order_by(Board.id.asc()) \
+                .limit(limit) \
+                .offset(offset) \
+                .all()
+        return res
+    except SQLAlchemyError as e:
+        return e
+
+# Board의 전체 갯수를 반환합니다
+def get_board_list_size() -> int | SQLAlchemyError:
+    try:
+        session = Session()
+        count = session.query(Board).count()
+        return count
+    except SQLAlchemyError as e:
+        return e
