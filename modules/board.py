65c03271b0610f2dd3c2852d690e7c4f013cf334
Orb-H <orb.cov@gmail.com>
2023-11-14 23:32:54 +0900

    Add body of board_get logic function

--- modules/board.py
+++ modules/board.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+
 import util.db as db
 from util.error_object import ErrorObject
 
@@ -34,17 +36,31 @@ def board_get(uid: str, board_id: int, offset: int, limit: int) -> dict | ErrorO
     #   dict | ErrorObject: board_info | ErrorObject
     # --------------------------------------------------
     # DB에서 board_id로 지정된 board의 metadata 반환
+    metadata = db.get_board_metadata(board_id)
+    if isinstance(metadata, db.SQLAlchemyError):
+        return ErrorObject(503, 'DB Error:' + metadata._message())
     
     # 결과가 없다면
+    if metadata == None:
         # return ErrorObject(404)
+        return ErrorObject(404, 'Board does not exist')
     
     # DB에서 board_id로 지정된 board의 post 중 ID 역순으로 offset부터 limit개의 게시글 목록 반환
+    posts = db.get_board_posts(board_id, offset, limit)
+    if isinstance(posts, db.SQLAlchemyError):
+        return ErrorObject(503, 'DB Error:' + posts._message())
     
     # 게시글 목록이 없다면
+    if len(posts) == 0:
         # 게시글 갯수 반환
+        posts_count = db.get_board_posts_count(board_id)
+        if isinstance(posts_count, db.SQLAlchemyError):
+            return ErrorObject(503, 'DB Error:' + posts_count._message())
         
         # offset이 게시글 갯수보다 크다면
+        if offset >= posts_count:
             # return ErrorObject(404)
+            return ErrorObject(404, 'Out of range')
     
     # return 게시판 정보
-    pass
+    return jsonify(metadata=metadata, posts=posts)
