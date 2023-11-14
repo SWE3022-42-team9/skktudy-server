348861526cd83315cab60f33cefeefa6b36431b3
Orb-H <orb.cov@gmail.com>
2023-11-14 19:42:03 +0900

    Add body of board_list logic function

--- modules/board.py
+++ modules/board.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+
 import util.db as db
 from util.error_object import ErrorObject
 
@@ -12,15 +14,24 @@ def board_list(uid: str, offset: int, limit: int) -> dict | ErrorObject:
     #   dict | ErrorObject: board_list | ErrorObject
     # --------------------------------------------------
     # DB에서 ID 역순으로 offset부터 limit개의 게시판 목록 반환
+    board_list = db.get_board_list(offset, limit)
+    if isinstance(board_list, db.SQLAlchemyError):
+        return ErrorObject(503, 'DB Error: ' + board_list._message())
 
     # 게시판 목록이 비었다면
+    if len(board_list) == 0:
         # 게시판 갯수 반환
+        board_size = db.get_board_list_size()
+        if isinstance(board_size, db.SQLAlchemyError):
+            return ErrorObject(503, 'DB Error: ' + board_size._message())
         
         # offset이 게시판 갯수보다 크다면
+        if offset >= board_size:
             # ErrorObject 반환(404)
+            return ErrorObject(404, 'Out of range')
 
     # return 게시판 목록
-    pass
+    return jsonify(data=board_list)
 
 # /board/{BOARD_ID}
 def board_get(uid: str, board_id: int, offset: int, limit: int) -> dict | ErrorObject:
