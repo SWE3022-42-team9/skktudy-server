fe5a1372773a686367ead9186990a369e0d722f9
Orb-H <orb.cov@gmail.com>
2023-11-15 17:10:10 +0900

    Add post_get related db function

--- util/db.py
+++ util/db.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
+from flask import jsonify
+
 from models import *
 
 engine = create_engine('') # TODO: Add database URL
@@ -30,3 +32,37 @@ def _execute_sql(sql: str) -> dict | SQLAlchemyError:
 # --------------------------------------------------
 
 # etc.
+
+# post_id에 해당하는 post가 존재하는지 확인
+def is_post_exist(post_id: int) -> bool | SQLAlchemyError:
+    res: bool | SQLAlchemyError
+
+    with Session() as session:
+        try:
+            res = session.query(Post) \
+                    .filter(Post.id == post_id) \
+                    .first() is not None
+        except SQLAlchemyError as e:
+            session.rollback()
+            res = e
+
+    return res
+
+def get_post_comments(post_id: int) -> dict | SQLAlchemyError:
+    res: dict | SQLAlchemyError = {}
+
+    with Session() as session:
+        try:
+            res = session.query(Post) \
+                    .filter(Post.id == post_id) \
+                    .first() \
+                    .to_dict()
+            
+            res["comments"] = session.query(Comment) \
+                    .filter(Comment.post_id == post_id) \
+                    .all()
+        except SQLAlchemyError as e:
+            session.rollback()
+            res = e
+
+    return res
