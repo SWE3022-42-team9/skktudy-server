6b748bb3d74822ca420f7f35989251c1c8edb6c8
Sebyeol23 <lixxil2323@gmail.com>
2023-11-15 13:20:02 +0900

    Implement get post info functions(db.py)

--- util/db.py
+++ util/db.py
@@ -30,3 +30,36 @@ def _execute_sql(sql: str) -> dict | SQLAlchemyError:
 # --------------------------------------------------
 
 # etc.
+
+# post_id로 지정된 post의 metadata를 반환합니다
+def get_post_metadata(post_id: int) -> Post | SQLAlchemyError | None:
+    res: Post | SQLAlchemyError | None
+    
+    with Session() as session:
+        try:
+            res = session.query(Post) \
+                    .filter(Post.id == post_id) \
+                    .first()
+        
+        except SQLAlchemyError as e:
+            session.rollback()
+            res = e
+    
+    return res
+
+# post_id로 지정된 post의 댓글 목록을 반환합니다
+def get_post_comments(post_id: int) -> List[Comment] | SQLAlchemyError:
+    res: List[Comment] | SQLAlchemyError
+    
+    with Session() as session:
+        try:
+            res = session.query(Comment) \
+                    .filter(Comment.post_id == post_id) \
+                    .order_by(Comment.id) \
+                    .all()
+        
+        except SQLAlchemyError as e:
+            session.rollback()
+            res = e
+
+    return res
