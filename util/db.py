b2c9ac5bc3ba6132dea2a0e2e1a1173548843ed3
Sebyeol23 <lixxil2323@gmail.com>
2023-11-15 14:23:50 +0900

    Implement comment like related db functions(db.py)

--- util/db.py
+++ util/db.py
@@ -30,3 +30,34 @@ def _execute_sql(sql: str) -> dict | SQLAlchemyError:
 # --------------------------------------------------
 
 # etc.
+
+def create_comment_like(new_comment_like: CommentLike) -> dict | SQLAlchemyError:
+    res: dict | SQLAlchemyError
+    
+    res = {"message": "Comment like successfully"}
+    with Session() as session:
+        try:
+            session.add(new_comment_like)
+            session.commit()
+        
+        except SQLAlchemyError as e:
+            session.rollback()
+            res = e
+    
+    return res
+
+#특정 댓글의 좋아요 수를 반환합니다.
+def get_comment_likes_count(comment_id: int) -> int | SQLAlchemyError:
+    res: int | SQLAlchemyError
+
+    with Session() as session:
+        try:
+            res = session.query(CommentLike) \
+                    .filter(CommentLike.comment_id == comment_id) \
+                    .count()
+        
+        except SQLAlchemyError as e:
+            session.rollback()
+            res = e
+    
+    return res
