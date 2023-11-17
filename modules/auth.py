578aaabfa02c634b5c86969e2ad8c2daadb7e030
Orb-H <orb.cov@gmail.com>
2023-11-17 11:01:02 +0900

    Implement token verification function

--- modules/auth.py
+++ modules/auth.py
@@ -17,18 +17,33 @@ def authenticate(token: str) -> str | ErrorObject:
     #   str | ErrorObject: uid | ErrorObject
     # --------------------------------------------------
     # DB에서 token에 대한 uid 반환
+    uid = db.get_uid_from_token(token)
+    if isinstance(uid, db.SQLAlchemyError):
+        return ErrorObject(503, "DB Error: " + uid._message())
     
     # DB에 token이 없다면
+    if uid is None:
         # _renew() 호출해서 uid 반환
+        decoded_token = _renew(token)
         
         # uid가 ErrorObject라면
+        if isinstance(decoded_token, ErrorObject):
             # return ErrorObject
+            return decoded_token
+        
+        exists = db.is_user_exist(decoded_token['uid'])
+        if isinstance(exists, db.SQLAlchemyError):
+            return ErrorObject(503, "DB Error: " + exists._message())
         
         # uid가 DB에 없다면
+        if not exists:
             # DB에 uid 및 token 정보로 데이터 생성
+            result = db.add_user(decoded_token['uid'], decoded_token['name'], token)
+            if isinstance(result, db.SQLAlchemyError):
+                return ErrorObject(503, "DB Error: " + result._message())
 
     # return uid
-    pass
+    return uid
 
 # token에 해당하는 uid를 반환
 def _renew(token: str) -> dict | ErrorObject:
