b1a7e0f3a379e3f449a05f9e3f5a698fd2f09855
Orb-H <orb.cov@gmail.com>
2023-11-17 10:58:27 +0900

    Implement _renew

--- modules/auth.py
+++ modules/auth.py
@@ -31,18 +31,23 @@ def authenticate(token: str) -> str | ErrorObject:
     pass
 
 # token에 해당하는 uid를 반환
-def _renew(token: str) -> str | ErrorObject:
+def _renew(token: str) -> dict | ErrorObject:
     # TODO: Implement
     # parameters:
     #   token: str
     # returns:
     #   str | ErrorObject: uid | ErrorObject
     # --------------------------------------------------
+    try:
     # Firebase에 token을 전송하여 uid값 반환
+        decoded_token = auth.verify_id_token(token)
+        
+        return decoded_token
     
+    except Exception as e:
+        return ErrorObject(503, "Firebase Error: " + str(e))
     # uid가 없다면
         # return ErrorObject
     
     # uid가 있다면
         # return uid
-    pass
